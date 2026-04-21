b179bea0a717ba978f5705689205539f4d8f9c25
Patmc <patrick.mcisaac.dev@gmail.com>
2026-04-21 15:26:26 -0500

    re doing linked list to practice; ;

--- Pointers and Memory/Intermediate/binary_search.cpp
+++ Pointers and Memory/Intermediate/binary_search.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
-int binarySearch(int *count, int arr[], int target, int size)
+
+int binarySearch(int *arr, int size, int target)
 {
+
+    int result = -1;
+
     int l = 0;
     int r = size - 1;
-    std::cout << count << "     " << *count << std::endl;
+
     while (l <= r)
     {
         int mid = l + (r - l) / 2;
-        if (*(arr + mid) == target)
+
+        if (arr[mid] == target)
         {
-            *count += 1;
             return mid;
         }
-
-        if (*(arr + mid) < target)
+        else if (arr[mid] < target)
         {
             l = mid + 1;
-            *count += 1;
         }
         else
         {
             r = mid - 1;
-            *count += 1;
         }
     }
-    return -1;
+
+    return result;
 }
 int main()
 {
-    int count = 0;
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int result = binarySearch(&count, arr, 8, size);
+    int result = binarySearch(arr, size, 90);
 
-    std::cout << "found target at: " << result << " iterations: " << count << std::endl;
+    std::cout << result << std::endl;
+
     return 0;
 }
--- Pointers and Memory/Intermediate/linked_list.cpp
+++ Pointers and Memory/Intermediate/linked_list.cpp
@@ -5,3 +5,118 @@ attention to pointer-to-pointer (`Node**`) when modifying the head.
 */
 
 #include <iostream>
+
+class Node
+{
+
+  public:
+    int val;
+    Node *next;
+    Node(int v) : val(v), next(nullptr)
+    {
+    }
+    ~Node()
+    {
+    }
+};
+
+class LinkedList
+{
+  public:
+    Node *head;
+    int size;
+
+    LinkedList() : head(nullptr), size(0)
+    {
+    }
+    ~LinkedList()
+    {
+        Node *current = head;
+        while (current)
+        {
+            Node *r = current;
+            current = r->next;
+            delete r;
+        }
+    }
+
+    void insert(int val)
+    {
+        Node *node = new Node(val);
+        if (head == nullptr)
+        {
+            head = node;
+        }
+        else
+        {
+            node->next = head;
+            head = node;
+        }
+        size += 1;
+    }
+
+    int insert(int val, int index)
+    {
+        if (index > size)
+        {
+            return -1;
+        }
+        Node *node = new Node(val);
+        int count = 0;
+        if (head == nullptr)
+        {
+            head = node;
+            return;
+        }
+
+        Node *current = head;
+
+        while (count < index - 1)
+        {
+            current = current->next;
+            count++;
+        }
+        Node *next = current->next;
+        current->next = node;
+        node->next = next;
+        size += 1;
+        return;
+    }
+
+    void remove(int val)
+    {
+        Node *current = head;
+        if (current->val == val)
+        {
+            Node *r = current;
+            head = current->next;
+            delete r;
+            size -= 1;
+            return;
+        }
+        while (current->next)
+        {
+            if (current->next->val == val)
+            {
+                Node *r = current->next;
+                current->next = current->next->next;
+                delete r;
+                size -= 1;
+                return;
+            }
+            current = current->next;
+        }
+        return;
+    }
+};
+
+int main()
+{
+    LinkedList list = LinkedList();
+
+    list.insert(1);
+    list.insert(2);
+    list.insert(3, 2);
+    list.remove(1);
+    return 0;
+}
